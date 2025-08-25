c9149f4a237cca0cdd575606515e29a9f80a3b4e
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2025-08-24 20:30:18 -0400

    update to hotel block component

--- src/Components/HotelBlock.js
+++ src/Components/HotelBlock.js
@@ -1,36 +1,34 @@
 import React from "react";
+import hotelData from "../hotels.json"; 
 
 const HotelBlock = () => {
   return (
     <div className="bg-weddingPeach min-h-screen p-4 text-center">
       <h2 className="text-2xl font-bold text-center mb-4">Hotel Block</h2>
       <ul className="max-w-md mx-auto bg-white p-4 rounded shadow">
-        <a
-          target="_blank"
-          href="https://www.hiexpress.com/redirect?path=rates&brandCode=EX&localeCode=en&regionCode=1&hotelCode=DTTOM&checkInDate=19&checkInMonthYear=022026&checkOutDate=21&checkOutMonthYear=022026&_PMID=99801505&GPC=ENW&cn=no&viewfullsite=true"
-          className="block group"
-        >
-          <li className="mb-4 border border-black p-3 m-1 shadow transform transition-all duration-300 group-hover:scale-105 group-hover:p-4">
-            <h3 className="font-bold underline">
-              Holiday Inn Express & Suites Troy
-            </h3>
-            <p>400 Stephenson Highway</p>
-            <p>Troy, MI 48083</p>
-            <p>(123) 456-7890</p>
-          </li>
-        </a>
-        <a
-          target="_blank"
-          href="https://www.hilton.com/en/book/reservation/rooms/?ctyhocn=DTTTRES&arrivalDate=2026-03-19&departureDate=2026-03-21&groupCode=CES90K&room1NumAdults=1&cid=OM%2CWW%2CHILTONLINK%2CEN%2CDirectLink"
-          className="block group"
-        >
-          <li className="mb-4 border border-black p-3 m-1 shadow transform transition-all duration-300 group-hover:scale-105 group-hover:p-4">
-            <h3 className="font-bold">Embassy Suites by Hilton</h3>
-            <p>850 Tower Dr</p>
-            <p>Troy, MI 48098</p>
-            <p>(123) 456-7890</p>
-          </li>
-        </a>
+        {hotelData.map((hotel) => (
+          <a
+            key={hotel.id}
+            target="_blank"
+            rel="noopener noreferrer"
+            href={hotel.url}
+            className="block group"
+          >
+            <li
+              className="mb-4 border border-black p-3 m-1 shadow rounded-lg 
+                         transition-all duration-300 ease-in-out 
+                         hover:bg-gray-100 hover:shadow-lg cursor-pointer"
+            >
+              <h3 className="font-bold group-hover:underline">
+                {hotel.name}
+              </h3>
+              <p>{hotel.address}</p>
+              <p>{hotel.city}</p>
+              <p>{hotel.phone}</p>
+              <button className="w-3/5 m-2 py-2  bg-pink-600 text-white rounded hover:bg-pink-600">Book</button>
+            </li>
+          </a>
+        ))}
 
         <h4 className="pb-3">
           The final day for guests to book at your discounted group rate is Feb
