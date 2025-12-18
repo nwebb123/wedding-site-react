ee0b61509bb007fe83c61c2e79ea258049736ddb
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2025-12-17 19:51:36 -0500

    hotfix: adding address to gift registry page

--- src/Components/GiftRegistry.js
+++ src/Components/GiftRegistry.js
@@ -5,9 +5,9 @@ const GiftRegistry = () => {
     <div className="bg-weddingPeach min-h-screen p-4">
       <h2 className="text-2xl font-bold text-center mb-4">Gift Registry</h2>
       <div className="max-w-md mx-auto">
-        <p className="text-center mb-4">
+        <h5 className="text-center mb-4">
           View our registry:
-        </p>
+        </h5>
         <a
           href="https://www.amazon.com/wedding/registry/1L70VWN7ZOF7P"
           target="_blank"
@@ -15,7 +15,10 @@ const GiftRegistry = () => {
           className="block text-center bg-pink-600 text-white py-2 rounded shadow hover:bg-pink-500"
         >
           Amazon Wishlist
-        </a>       
+        </a>
+        <h5 class="text-center m-4" >
+          If you would like to send the gift directly to us you can do so at <span class="underline">48885 Bemis Rd, Sumpter Township MI 48111</span>
+        </h5>       
       </div>
     </div>
   );
